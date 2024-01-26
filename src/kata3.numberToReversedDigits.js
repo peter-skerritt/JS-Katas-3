24334f28a7ddb3fae783987c727e0fcdcf5033d3
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-01-26 15:43:24 +0000

    Finished reversed numbers kata.

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+
+const array = [1,2,3,4,5];
+const reversedArray = array.reverse();
+
+return reversedArray;
+};
 
 module.exports = numberToReversedDigits;
